603159541ab5dc854f22e1b97a6d39f699a8651a
Dan Waxman <dan.waxman1@gmail.com>
2020-05-05 01:05:18 -0400

    Added debug mode and added some semicolons
    "Debug mode" refers to whether FFmpeg output is displayed.

--- splitter.js
+++ splitter.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const {
     spawn
-} = require('child_process')
+} = require('child_process');
 const process = require('process');
+
+// Should debug statements be printed?
+const debug = false;
 
 // Get video number from command line
 const videoNumber = process.argv[2];
@@ -16,7 +19,7 @@ var basePath = `/home/danwaxman/devel/Work/Hemmick/${process.argv[3]}_Hemmick_Le
 var SplitFolders = basePath + "SplitFolders";
 var SourceLectures = basePath + "SourceLectures";
 
-var csvName = `csv/${process.argv[3]}/${videoNumber}.csv`
+var csvName = `csv/${process.argv[3]}/${videoNumber}.csv`;
 var index = 0;
 var runFfmpeg = true;
 
@@ -29,7 +32,7 @@ fs.createReadStream(csvName)
             makenewVideo(row);
         })
     .on('end', function() {
-        console.log("Finished processing.")
+        console.log("Finished processing.");
     });
 
 /**
@@ -54,20 +57,20 @@ function toSeconds(atime) {
  */
 function makenewVideo(row) {
     // Video numbers should be padded (i.e. Lecture##.mp4)
-    fileName = `Lecture${videoNumber.padStart(2, '0')}.mp4`
+    fileName = `Lecture${videoNumber.padStart(2, '0')}.mp4`;
     // Elements of row should include 'Topic', 'Subtopic', 'Filename', 'Video Start Time', and 'Video End Time'
-    console.log(row)
+    if (debug) console.log(row);
 
-    var recompress = ['-codec', 'copy'];;
-    var startTimeSeconds = toSeconds(row['Video Start Time'])
-    var endTimeSeconds = toSeconds(row['Video End Time'])
+    var recompress = ['-codec', 'copy'];
+    var startTimeSeconds = toSeconds(row['Video Start Time']);
+    var endTimeSeconds = toSeconds(row['Video End Time']);
     if (row['recompress']) {
         recompress = ['-vcodec', 'libx264'];
-        console.log("recompressing", ...recompress)
+        console.log("recompressing", ...recompress);
     }
 
     // Output filename
-    var currentVideo = `${SplitFolders}/${row['Topic']}/${row['Subtopic']}`
+    var currentVideo = `${SplitFolders}/${row['Topic']}/${row['Subtopic']}`;
 
     // Create new video in filesystem
     fs.mkdir(currentVideo, {
@@ -85,11 +88,11 @@ function makenewVideo(row) {
                 });
 
                 ffmpeg.stderr.on('data', (data) => {
-                    console.log(`stderr: ${data}`);
+                    if (debug) console.log(`stderr: ${data}`);
                 });
 
                 ffmpeg.on('close', (code) => {
-                    console.log(`child process exited with code ${code}`);
+                    if (debug) console.log(`child process exited with code ${code}`);
                 });
             }
         }
